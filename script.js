7107e74b70085d08c3f64cc92f3598d74c59e40c
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-13 18:45:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-//complete this code
 class Person {
 	constructor(name, age){
-		this._name = name;
-		this._age = age;
+		this.name = name;
+		this.age = age;
 	}
 
 	get getName(){
-		return this._name;
+		return this.name;
 	}
 
 	set setAge(age){
-		this._age = age;
+		this.age = age;
 	}
 }
 
 class Student extends Person {
 	get study(){
 		// console.log();
-		return this._name + "is studying";
+		return this.name + " is studying";
 	}
 }
 
 class Teacher extends Person {
 	get teach(){
 		// console.log();
-		return this._name + "is teaching";
+		return this.name + " is teaching";
 	}
 }
-
-// const t1 = new Teacher("john" , 25);
-// t1.teach();
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
