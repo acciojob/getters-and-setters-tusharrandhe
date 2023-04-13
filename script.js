cb879ab86a6e1f1e026eedaa71ba3f79acf0a3e2
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-13 20:02:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 class Person {
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
-
-	get getName(){
-		return this.name;
-	}
-
-	set setAge(age){
-		this.age = age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  
+  set age(age) {
+    this._age = age;
+  }
 }
-
+ 
 class Student extends Person {
-	study(){
-		console.log(this.name + " is studying");
-		// return ;
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
-
+ 
 class Teacher extends Person {
-	teach(){
-		console.log(this.name + " is teaching");
-		// return ;
-	}
-	
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-// Do not change the code below this line
+ 
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
